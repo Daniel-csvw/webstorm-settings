f0c537161c1c2ee99c1fd1440e3a68dd54df9a33
LuHui2 <luhui2@csvw.com>
2019-11-01 12:56:12 +0800

    WS-2019.1.3 <luhui2@tets1067 Create baseRefactoring.xml, Vue Single File Component.vue, ide.general.xml, Default.xml, project.default.xml, JavaScript.xml, laf.xml, Default.xml, find.xml, filetypes.xml, projectView.xml, editor.codeinsight.xml, editor.xml, Vue.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,19 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script>
+export default {
+  name: "${COMPONENT_NAME}",
+  data(){
+    return {
+      items:[]
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
